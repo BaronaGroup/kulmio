c4a95663ac40269f0041e5cb2cf6859819db0607
Tapani Haka <tapani.haka@barona.fi>
2022-10-24 17:39:45 +0300

    Leave filter controls visible while scrolled

--- src/ui/LogView/LogView.tsx
+++ src/ui/LogView/LogView.tsx
@@ -11,11 +11,13 @@ export const LogView: React.FC = () => {
   return (
     <div className="flex fixed left-0 right-0 bottom-0 top-0">
       <LogControls />
-      <div className="grow flex flex-col overflow-y-auto" ref={setScrollElement}>
+      <div className="grow flex flex-col overflow-y-auto">
         <LogFilter />
-        <LogDataProvider>
-          <LogData scrollElement={scrollElement} />
-        </LogDataProvider>
+        <div className={'overflow-y-auto'} ref={setScrollElement}>
+          <LogDataProvider>
+            <LogData scrollElement={scrollElement} />
+          </LogDataProvider>
+        </div>
       </div>
     </div>
   )
